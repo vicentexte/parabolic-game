df60ecc6cb120d870eb80b8954a8fe857d3c3dcf
Claudio153 <diazclaudio153@gmail.com>
2025-05-20 21:46:33 -0400

    edit phasergame

--- src/components/PhaserGame.vue
+++ src/components/PhaserGame.vue
@@ -29,8 +29,8 @@ export default{
         let player
         const config = {
             type: Phaser.AUTO,
-            width: 800,
-            height: 600,
+            width: window.innerWidth,
+            height: window.innerHeight,
             physics: {
             default: 'arcade',
                 arcade: {
