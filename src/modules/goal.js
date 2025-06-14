a7682c8984287d5bdae33cabc84375d00d9bc19d
Vicente <vicentexte@gmail.com>
2025-06-13 23:38:02 -0400

    nothing

---
+++ src/modules/goal.js
@@ -0,0 +1,16 @@
+export default class Goal {
+    constructor(scene,x,y){
+        this.scene = scene;
+        this.x = x;
+        this.y = y;
+        this.imagen = scene.add
+    }
+
+    create(){
+
+    }
+
+    invertColor(){
+
+    }
+}
